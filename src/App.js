12ec0ce48bd2f8f645c6a0097d64fa87b37f9052
srm-laufire <srm@laufire.in>
2021-10-11 11:53:51 +0000

    Bug fixed code was not working.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { React } from 'react';
 import './App.scss';
 import Name from './components/name';
+import Age from './components/age';
 
 const App = (context) =>
-	<div role="App" className="App">Name { Name(context) }</div>;
+	<div role="App" className="App">
+		Name { Name(context) }
+		Age { Age(context) }
+	</div>;
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,18 @@ import { rndString } from '@laufire/utils/random';
 import App from './App';
 import { React } from 'react';
 import * as Name from './components/name';
+import * as Age from './components/age';
 import context from './core/context';
 
 describe('App', () => {
 	test('renders the component appropriately', () => {
 		const name = rndString();
+		const age = rndString();
 
 		jest.spyOn(Name, 'default')
 			.mockImplementation(() => <div role={ name }/>);
+		jest.spyOn(Age, 'default')
+			.mockImplementation(() => <div role={ age }/>);
 
 		const { getByRole } = render(App(context));
 
@@ -20,6 +24,8 @@ describe('App', () => {
 		expect(component).toBeInTheDocument();
 		expect(component).toHaveClass('App');
 		expect(component).toHaveTextContent('Name');
+		expect(nameComponent).toBeInTheDocument();
+		expect(Name.default).toHaveBeenCalledWith(context);
 		expect(nameComponent).toBeInTheDocument();
 		expect(Name.default).toHaveBeenCalledWith(context);
 	});
